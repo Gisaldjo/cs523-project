41fca7b659512a63056ef4fc778408a3a8bff51f
gp27 <gp27@localhost>
2024-05-10 13:35:23 -0500

    more logs

--- collect_logs.sh
+++ collect_logs.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 timestamp=$(date +"%Y-%m-%d_%H%M%S")
+prefix=${1:-v2}
 
-mkdir logs/mixed_$timestamp
-mv cpu_experiment/logs/* logs/mixed_$timestamp/
-mv network_experiment/logs/* logs/mixed_$timestamp/
-mv output.log logs/mixed_$timestamp/
+mkdir logs/${prefix}_mixed_$timestamp
+mv cpu_experiment/logs/* logs/${prefix}_mixed_$timestamp/
+mv network_experiment/logs/* logs/${prefix}_mixed_$timestamp/
+mv output.log logs/${prefix}_mixed_$timestamp/

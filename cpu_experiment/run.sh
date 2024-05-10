94b4aa3e33a5f47e110978b59c90af045cea8242
average-avancha <rra2@illinois.edu>
2024-05-09 22:40:15 -0500

    stashing progress

--- cpu_experiment/run.sh
+++ cpu_experiment/run.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
 
-sudo docker built -t cpu_experiment .
-sudo docker run -it --rm cpu_experiment
+# File path
+file=".index.txt"
+
+# Check if file exists and is not empty
+if [ -s "$file" ]
+then
+    # Read the number from the file
+    index=$(<"$file")
+    # Increment the number
+    ((index++))
+else
+    # Initialize index if file does not exist or is empty
+    index=1
+fi
+
+# Write the new index back to the file
+echo "$index" > "$file"
+
+if [ ! -d "data" ]; then
+    mkdir data
+fi
+
+
+sudo docker build -t cpu_experiment .
+# sudo docker built -t cpu_experiment .
+
+sudo docker run -it --rm cpu_experiment >> "data/run_$index.log"
--- network_experiment/run.sh
+++ network_experiment/run.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+# File path
+file=".index.txt"
+
+# Check if file exists and is not empty
+if [ -s "$file" ]
+then
+    # Read the number from the file
+    index=$(<"$file")
+    # Increment the number
+    ((index++))
+else
+    # Initialize index if file does not exist or is empty
+    index=1
+fi
+
+# Write the new index back to the file
+echo "$index" > "$file"
+
+if [ ! -d "data" ]; then
+    mkdir data
+fi
+
 # Check if the netserver container is running; if not, start it
 if [ "$(sudo docker ps -q -f name=netserver)" ]; then
     echo "Netserver is already running."
@@ -15,4 +37,4 @@ unset NETSERVER_IP
 NETSERVER_IP=$(sudo docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' netserver)
 echo "Netserver IP: $NETSERVER_IP"
 # Start the netperf client container
-sudo docker run  -it --rm networkstatic/netperf -H $NETSERVER_IP
+sudo docker run  -it --rm networkstatic/netperf -H $NETSERVER_IP >> "data/run_$index.log"

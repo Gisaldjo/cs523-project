e3c823d5b464342033cdd0b96f5e74c0b826ef44
gp27 <gp27@node0.gp27-206409.cs523-uiuc-sp24-pg0.wisc.cloudlab.us>
2024-05-10 19:09:37 -0500

    rename log dirs

--- collect_logs.sh
+++ collect_logs.sh
@@ -3,10 +3,10 @@
 timestamp=$(date +"%Y-%m-%d_%H%M%S")
 prefix=${1:-v2}
 
-mkdir logs/${prefix}_mixed_$timestamp
-mv cpu_experiment/logs/* logs/${prefix}_mixed_$timestamp/
-mv network_experiment/logs/* logs/${prefix}_mixed_$timestamp/
-mv output.log logs/${prefix}_mixed_$timestamp/
-mkdir logs/${prefix}_mixed_$timestamp/softirq/
-mv softirq_logs/* logs/${prefix}_mixed_$timestamp/softirq/
-mv cpu_utilization.log logs/${prefix}_mixed_$timestamp/
+mkdir logs/${prefix}_$timestamp
+mv cpu_experiment/logs/* logs/${prefix}_$timestamp/
+mv network_experiment/logs/* logs/${prefix}_$timestamp/
+mv output.log logs/${prefix}_$timestamp/
+mkdir logs/${prefix}_$timestamp/softirq/
+mv softirq_logs/* logs/${prefix}_$timestamp/softirq/
+mv cpu_utilization.log logs/${prefix}_$timestamp/
--- experiment_run.sh
+++ experiment_run.sh
@@ -1,8 +1,8 @@
 ./cpu_utilization.sh &
 CPU_UTIL_PID=$!
 ./softirq.sh &
-# for i in {1..8}
-for i in $(seq 20 20 80)
+# for i in $(seq 20 20 80)
+for i in $(seq 4 4 32)
 do
     echo "---------------------"
     echo "$(date) - Running experiment round $i"
@@ -10,8 +10,8 @@ do
     cd network_experiment
     ./run.sh $i
 
-    # cd ../cpu_experiment
-    # ./run.sh $i
+    cd ../cpu_experiment
+    ./run.sh $i
     cd ../
     sleep 30
     while [ $(sudo docker ps -a | wc -l) -gt 2 ]
